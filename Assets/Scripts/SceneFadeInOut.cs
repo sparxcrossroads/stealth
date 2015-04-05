920d4fcc5cf6932a4b75762a8d54ec4439928c4f
sparxcrossroads <kisshotblade@sina.com>
2015-04-05 17:30:20 +0800

    屏幕渐变效果
    guitexture设置成 enable 会停止渲染 减少资源消耗

---
+++ Assets/Scripts/SceneFadeInOut.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour {
+
+    public float fadeSpeed = 1.5f;
+    private bool sceneStarting = true;
+
+	void Awake()
+    {
+        this.guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FadeToClear()
+    {
+        this.guiTexture.color = Color.Lerp(this.guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+    void FadeToBlack()
+    {
+        this.guiTexture.color = Color.Lerp(this.guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+
+    void StartScene()
+    {
+        FadeToClear();
+        if(this.guiTexture.color.a<=0.05f)
+        {
+            //把纹理颜色直接设为透明 然后禁用纹理 从而停止渲染 防止占用资源
+            this.guiTexture.color = Color.clear;
+            this.guiTexture.enabled = false;
+
+            sceneStarting = false;
+        }
+    }
+    public void EndScene()
+    {
+        this.guiTexture.enabled = true;
+
+        FadeToBlack();
+
+        if (guiTexture.color.a >= 0.95f)
+            Application.LoadLevel(0);
+    }
+}
