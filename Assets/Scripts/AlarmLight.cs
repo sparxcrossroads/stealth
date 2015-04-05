0bfbfff37337c74e11f8f4cd76a989dfea7b8e41
sparxcrossroads <kisshotblade@sina.com>
2015-04-05 16:21:00 +0800

    警报灯设置
    AlarmLight脚本的编写 (个人添加了unityvs 插件)
    其中关于最大值和最小值的来回变化写法值得借鉴
    
    prop中 megaphone 添加 audioclip

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+
+    public float fadeSpeed=2f;
+    public float hightIntensity=3f;
+    public float lowIntensity=0.5f;
+    public float changeMargin = 0.3f;
+    public bool alarmOn;
+    
+    private float targetIntensity;
+
+    void Awake()
+    {
+        light.intensity = 0f;
+        targetIntensity = hightIntensity;
+    }
+
+    void Update() 
+    {
+        if (alarmOn)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+            CheckTargetIntensity();
+        }
+        else
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 0f, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    void CheckTargetIntensity()
+    {
+        if(Mathf.Abs(light.intensity-targetIntensity)<changeMargin)
+        {
+            if (targetIntensity == hightIntensity)
+            {
+                targetIntensity = lowIntensity;
+            }
+            else
+            {
+                targetIntensity = hightIntensity;
+            }
+        }
+    }
+}
